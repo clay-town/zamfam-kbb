d1925d04e0ad44e0006f8892d7e9f7e2cd727fba
clay-town <justinmccune@icloud.com>
2019-11-27 14:59:05 -0800

    Initialize PORT variable

--- cmd/zamfamkbb/main.go
+++ cmd/zamfamkbb/main.go
@@ -3,13 +3,15 @@ package main
 import (
 		"log"
 		"net/http"
+		"os"
+		"github.com/joho/godotenv"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-
+	godotenv.Load()
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), mux)
 	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/clay-town/trade-wars
 
 go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/joho/godotenv v1.3.0
+)
