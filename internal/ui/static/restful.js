15090945d9f55a887ab07d977394dbc26683b07b
justin@mazamamedia.com <justinmccune@icloud.com>
2019-12-02 18:00:13 -0800

    Implement fetchModels

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -1,8 +1,22 @@
 
 function fetchMakes(year=1992) {
-	alert("Fetching makes")
   var request = new XMLHttpRequest();
   var url = "/make?year="+year;
+
+  request.open('POST', url, true);
+  request.onload = function(){
+    var data = JSON.parse(this.response);
+    console.log(JSON.parse(data));
+    // access JSON data
+    // populate drop down menue with makes and hidden IDs
+
+  }
+  request.send();
+}
+
+function fetchModels(makeId=18) {
+  var request = new XMLHttpRequest();
+  var url = "/model?makeid="+makeId;
 
   request.open('POST', url, true);
   request.onload = function(){
