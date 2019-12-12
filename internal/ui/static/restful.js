43bbcfff4ce3a8eda4f2020d131b8b67b7ccec23
justin@mazamamedia.com <justinmccune@icloud.com>
2019-12-11 17:16:45 -0800

    Add fetch value functionality

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -12,7 +12,7 @@ function populateDropDowns(){
   document.getElementById("yeardd").innerHTML = yearMenu
 }
 
-function fetchMakes(year=1992) {
+function fetchMakes(year) {
   var request = new XMLHttpRequest();
   var url = "/make?year="+year;
 
@@ -22,7 +22,7 @@ function fetchMakes(year=1992) {
     data = JSON.parse(data);
     // access JSON data
     // populate drop down menue with makes and hidden IDs
-    var makeMenu = "<select name='makedd' onchange='fetchModels(this.value)'>"+
+    var makeMenu = "<select name='makedd' onchange='fetchModels("+year+", this.value)'>"+
                     "<option value=''>Make</option>";
     for (x = 0; x < data.items.length; x++){
       makeMenu = makeMenu+"<option value="+data.items[x].makeId+">"+data.items[x].makeName+"</option>";
@@ -34,21 +34,14 @@ function fetchMakes(year=1992) {
   request.send();
 }
 
-function fetchModels(makeid=18) {
+function fetchModels(year, makeid) {
   var request = new XMLHttpRequest();
   var url = "/model?makeid="+makeid;
-
+  var makeid = makeid
   request.open('POST', url, true);
   request.onload = function(){
     var data = JSON.parse(JSON.parse(this.response));
-    
-    console.log(data.items)
-    //console.log("Make Id:  ",data.items[0].makeId)
-    //console.log("Make Name:  ",data.items[0].makeName)
-    //console.log(data.items.length)
-    // access JSON data
-    // populate drop down menue with makes and hidden IDs  
-    var modelMenu = "<select name='modeldd' onchange='fetchTrims(this.value)'>"+
+    var modelMenu = "<select name='modeldd' onchange='fetchTrims("+year+", "+makeid+", this.value)'>"+
                     "<option value=''>Model</option>";
     for (x = 0; x < data.items.length; x++){
       modelMenu = modelMenu+"<option value="+data.items[x].modelId+">"+data.items[x].modelName+"</option>";
@@ -59,44 +52,64 @@ function fetchModels(makeid=18) {
   request.send();
 }
 
-function fetchTrims(modelid=122, yearid=1992) {
+function fetchTrims(year, makeid, modelid) {
   var request = new XMLHttpRequest();
-  var url = "/trim?modelid="+modelid+"&yearid="+yearid;
+  var url = "/trim?modelid="+modelid+"&yearid="+year;
 
   request.open('POST', url, true);
   request.onload = function(){
-    var data = JSON.parse(this.response);
-    console.log(JSON.parse(data));
+    var data = JSON.parse(JSON.parse(this.response));
+    console.log(data)
     // access JSON data
-    // populate drop down menue with makes and hidden IDs    
+    // populate drop down menue with makes and hidden IDs  
+    var trimMenu = "<select name='trimdd' onchange='fetchVehiclesByYMMT("+year+", "+makeid+", "+modelid+", this.value)'>"+
+                    "<option value=''>Trim</option>";
+    for (x = 0; x < data.items.length; x++){
+      trimMenu = trimMenu+"<option value="+data.items[x].trimId+">"+data.items[x].trimName+"</option>";
+    } 
+    trimMenu = trimMenu+"</select>";
+    document.getElementById("trimdd").innerHTML = trimMenu
+
   }
   request.send();
 }
 
-function fetchVehiclesByYMMT(yearid=1992, makeid=18, modelid=122, trimid=261475) {
+function fetchVehiclesByYMMT(year, makeid, modelid, trimid) {
 	var request = new XMLHttpRequest();
- 	var url = "/vehicle?modelid="+modelid+"&yearid="+yearid+"&makeid="+makeid+"&trimid="+trimid;
+ 	var url = "/vehicle?modelid="+modelid+"&yearid="+year+"&makeid="+makeid+"&trimid="+trimid;
 
 	request.open('POST', url, true);
  	request.onload = function(){
- 		var data = JSON.parse(this.response);
- 		console.log(JSON.parse(data));
-    	// access JSON data
-    	// populate drop down menue with makes and hidden IDs
+ 		var data = JSON.parse(JSON.parse(this.response));
+ 		console.log(data.items[0].vehicleId)
+
+    // fetch value
+    fetchValue(year, makeid, modelid, trimid, data.items[0].vehicleId)
   	}
   	request.send();
 }
 
-function fetchValue(vehicleid=12155) {
+function fetchValue(year, makeid, modelid, trimid, vehicleid) {
   var request = new XMLHttpRequest();
   var url = "/value?vehicleid="+vehicleid;
 
 request.open('POST', url, true);
   request.onload = function(){
-    var data = JSON.parse(this.response);
-    console.log(JSON.parse(data));
-      // access JSON data
-      // populate drop down menue with makes and hidden IDs
+    var data = JSON.parse(JSON.parse(this.response));
+    console.log(data);
+    var fairV = data.prices[0].rangeLow + " - "+data.prices[0].rangeHigh
+    var goodV = data.prices[1].rangeLow + " - "+data.prices[1].rangeHigh
+    var excellentV = data.prices[2].rangeLow+" - "+data.prices[2].rangeHigh
+    var veryGoodV = data.prices[3].rangeLow+" - "+data.prices[3].rangeHigh
+
+    valueDisplay = "<table>  <tr><td>Fair:      </td> <td>"+fairV+"</td></tr>"+
+                            "<tr><td>Good:      </td> <td>"+goodV+"</td></tr>"+
+                            "<tr><td>Very Good: </td> <td>"+veryGoodV+"</td></tr>"+
+                            "<tr><td>Excellent: </td> <td>"+excellentV+"</td></tr> </table>";
+
+    document.getElementById("value").innerHTML = valueDisplay
+        // push value onto screen
+        // send info back into manychat....somehow
     }
     request.send();
 }
