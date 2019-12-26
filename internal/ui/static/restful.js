238ad54dd980b48e70b15f468efa60a5c44d3e83
boykj <jboyk918@gmail.com>
2019-12-26 13:20:33 -0800

    restful.js: Remove LOC for window closing

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -132,8 +132,8 @@ request.open('POST', url, true);
                     // self.close();
                     // window.open('','_self').close()
 
-                    window.open('','_parent','');
-                    window.close()
+                    window.open('','_parent','').close()
+                    // window.close()
                   //  var imgURL = "https://ih1.redbubble.net/image.665615842.5354/mp,840x830,matte,f8f8f8,t-pad,750x1000,f8f8f8.u1.jpg"
                     //var closeURL="https://www.messenger.com/closeWindow/?image_url="+imgURL+"&display_text="+"helloworld"
                     //window.location.replace(closeURL);
