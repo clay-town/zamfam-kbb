f9664a62f4475adfbd0b57ab0e078ea7d56d1050
boykj <jboyk918@gmail.com>
2019-12-26 13:18:04 -0800

    restful.js: Minor changes for window closing

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -127,10 +127,13 @@ request.open('POST', url, true);
                     request.open('POST', url, true);
                     request.onload = function(){
                       // close window
-                    window.opener = self;
+                    // window.opener = self;
                     //window.close();
-                    self.close();
-                    window.open('','_self').close()
+                    // self.close();
+                    // window.open('','_self').close()
+
+                    window.open('','_parent','');
+                    window.close()
                   //  var imgURL = "https://ih1.redbubble.net/image.665615842.5354/mp,840x830,matte,f8f8f8,t-pad,750x1000,f8f8f8.u1.jpg"
                     //var closeURL="https://www.messenger.com/closeWindow/?image_url="+imgURL+"&display_text="+"helloworld"
                     //window.location.replace(closeURL);
