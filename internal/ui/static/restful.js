18265a2ab7591061720c117f7163a60138413c05
boykj <jboyk918@gmail.com>
2020-01-30 08:16:26 -0800

    restful.js: Add code for web window closing

--- internal/ui/static/restful.js
+++ internal/ui/static/restful.js
@@ -129,10 +129,14 @@ request.open('POST', url, true);
                  
                     request.open('POST', url, true);
                     request.onload = function(){
-                    // close window
-                    // window.opener = self;
-                    // window.close();
-                    // self.close();
+                      var testWindow = web_window.self;
+                      testWindow.opener = window.self
+                      testWindow.close():
+
+
+                    //window.opener = self;
+                    //web_window.close();
+                    //self.close();
                     // window.open('','_self').close()
 
                     // var imgURL="https://ih1.redbubble.net/image.665615842.5354/mp,840x830,matte,f8f8f8,t-pad,750x1000,f8f8f8.u1.jpg"
